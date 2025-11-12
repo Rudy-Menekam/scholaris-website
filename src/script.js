050d966b871fe2cfe8cafb148249b28596da9f12
Rudy-Menekam <menekamrudy@gmail.com>
2025-11-12 15:17:57 +0100

    Change logo

--- src/script.js
+++ src/script.js
@@ -1,27 +1,5 @@
 import './style.css'
 
-  document.addEventListener('alpine:init', () => {
-    Alpine.store('accordion', {
-      tab: 0
-    });
-
-    Alpine.data('accordion', (idx) => ({
-      init() {
-        this.idx = idx;
-      },
-      idx: -1,
-      handleClick() {
-        this.$store.accordion.tab = this.$store.accordion.tab === this.idx ? 0 : this.idx;
-      },
-      handleRotate() {
-        return this.$store.accordion.tab === this.idx ? 'rotate-180' : '';
-      },
-      handleToggle() {
-        return this.$store.accordion.tab === this.idx ? `max-height: ${this.$refs.tab.scrollHeight}px` : '';
-      }
-    }));
-  })
-
    // --- Mobile Menu Toggle ---
   const menuBtn = document.getElementById("menu-btn");
   const mobileMenu = document.getElementById("mobile-menu");
